44b19908d7b73c625a71ff1cc9755ed525cea664
OGURA DAIKI <dkog2001@gmail.com>
2023-02-03 00:13:55 +0900

    タイルのループを分離

--- PadBoard.js
+++ PadBoard.js
@@ -1,8 +1,8 @@
 import ReactiveStates from "./ReactiveStates.js";
 
-class PADBoard extends HTMLElement{
+class PADBoard extends HTMLElement {
 
-  static get style(){
+  static get style() {
     return `
     :host{
       display:block;
@@ -19,21 +19,24 @@ class PADBoard extends HTMLElement{
 
   #canvas;
   #states;
-  #tileSize=128;
-  constructor(){
+  #tileSize = 128;
+  constructor() {
     super();
-    this.attachShadow({mode:"open"});
+    this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <style>${this.constructor.style}</style>
       <canvas id="canvas"></canvas>
     `;
 
     this.#states = ReactiveStates({
-      size:{
-        value:6,
+      size: {
+        value: 6,
       },
+      board: {
+        value: [...Array(5)].map(() => [...Array(6)].map(() => Math.floor(Math.random() * 10))),
+      }
     });
-    this.#states.setCallback(()=>this.render());
+    this.#states.setCallback(() => this.render());
 
     this.#canvas = this.shadowRoot.querySelector("#canvas");
 
@@ -41,33 +44,44 @@ class PADBoard extends HTMLElement{
   }
 
   #start;
-  set start(board){
+  set start(board) {
     this.#start = board;
   }
 
-  set size(long){
-    this.#canvas.width = long*this.#tileSize;
-    this.#canvas.height = (long-1)*this.#tileSize;
-    this.#states.size = long;
+  set size(size) {
+    this.#canvas.width = size * this.#tileSize;
+    this.#canvas.height = (size - 1) * this.#tileSize;
+    const board = [...Array(size - 1)].map(() => [...Array(size)].map(() => Math.floor(Math.random() * 10)));
+    this.#states.updateStates({
+      size,
+      board
+    });
   }
 
-  #bgColor=['rgb(40, 20, 0)', 'rgb(60, 40, 0)'];
-  #drawBG(ctx){
+  #loopTile(callback) {
     const size = this.#states.size;
-    ctx.fillStyle = this.#bgColor[0];
-    ctx.fillRect(0, 0, size*this.#tileSize, size*this.#tileSize);
-
-    ctx.fillStyle = this.#bgColor[1];
-    for(let leftNeedle=0;leftNeedle<size;leftNeedle+=1){
-      for(let topNeedle=0;topNeedle<size-1;topNeedle+=1){
-        if((leftNeedle+topNeedle)%2){
-          ctx.fillRect(leftNeedle*this.#tileSize, topNeedle*this.#tileSize, this.#tileSize, this.#tileSize);
-        }
+    for (let top = 0; top < size - 1; top += 1) {
+      for (let left = 0; left < size; left += 1) {
+        callback({ top, left });
       }
     }
   }
 
-  render(){
+  #bgColor = ['rgb(40, 20, 0)', 'rgb(60, 40, 0)'];
+  #drawBG(ctx) {
+    const size = this.#states.size;
+    ctx.fillStyle = this.#bgColor[0];
+    ctx.fillRect(0, 0, size * this.#tileSize, size * this.#tileSize);
+
+    ctx.fillStyle = this.#bgColor[1];
+    this.#loopTile(({top, left})=>{
+      if ((left + top) % 2) {
+        ctx.fillRect(left * this.#tileSize, top * this.#tileSize, this.#tileSize, this.#tileSize);
+      }
+    })
+  }
+
+  render() {
     const ctx = this.#canvas.getContext("2d");
     this.#drawBG(ctx);
   }
