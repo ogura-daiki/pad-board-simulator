df65869225dc131eff968f81a81864b0c5f0920f
OGURA DAIKI <dkog2001@gmail.com>
2023-02-11 19:41:20 +0900

    ロックドロップと強化、弱化ドロップの描画を実装

--- src/libs/Drops.js
+++ src/libs/Drops.js
@@ -1,8 +1,11 @@
+const dropFilePathFromName = (name) => `${rootPath}/src/images/drops/${name}.png`;
+const dropFilePathFromId = (dropId) => dropFilePathFromName(dropNames[dropId]);
 const dropNames = [
   "fire", "water", "wood", "light", "dark", "heal",
   "poison", "deadlypoison", "trash", "bomb",
 ];
-const dropFilePath = (dropId) => `${rootPath}/src/images/drops/${dropNames[dropId]}.png`;
+const dropEffects = ["plus", "minus", "lock"];
+
 
 const loadImage = (filePath) => new Promise((res) => {
   const img = new Image();
@@ -11,22 +14,83 @@ const loadImage = (filePath) => new Promise((res) => {
   });
   img.src = filePath;
 });
-const dropImages = await Promise.allSettled(dropNames.map((v,id) => loadImage(dropFilePath(id)))).then(results=>results.map(r=>r.value));
+const loadImages = filePathList => Promise.allSettled(filePathList.map(path => loadImage(path))).then(results=>results.map(r=>r.value));
+const dropImages = await loadImages(dropNames.map((v, id)=>dropFilePathFromId(id)));
+const dropEffectImages = await loadImages(dropEffects.map(p=>dropFilePathFromName(p)))
 
 class Drop {
   constructor(dropId, {lock=false, power=0}={}){
     this.id = dropId;
-    this.lock = lock;
-    this.power = power;
+    this.lock = Math.random()>0.5;
+    this.power = Math.floor(Math.random()*3)-1;
   }
 
   draw(ctx, {size, x,y, hold}){
-    if(hold){
-      ctx.globalAlpha = 0.5;
+
+    const drawImage = (image, scale = 1) => {
+      const scaled = size*scale;
+      const offset = (scaled-size)/2;
+      const left = x * size - offset;
+      const top = y * size - offset;
+      ctx.drawImage(image, left, top, scaled, scaled);
     }
-    ctx.drawImage(dropImages[this.id], x * size, y * size, size, size);
-    ctx.globalAlpha = 1;
+
+    let alpha = 1;
+    const changeAlpha = (fn) => {
+      alpha = fn(alpha);
+      ctx.globalAlpha = alpha;
+    }
+    const decorateAlpha = (factor, drawDrop) =>()=>{
+      changeAlpha(a=>a*factor);
+      drawDrop();
+      changeAlpha(a=>a/factor);
+    }
+
+    const decoratePower = (drawDrop) => ()=>{
+      //強化ドロップ
+      if(this.power === 1){
+        ctx.filter = `brightness(120%)`;
+      }
+      //弱化ドロップ
+      if(this.power === -1){
+        ctx.filter = `brightness(70%)`;
+      }
+      drawDrop();
+      ctx.filter = "brightness(100%)";
+      
+      //強化ドロップ
+      if(this.power === 1){
+        drawImage(dropEffectImages[0]);
+      }
+      //弱化ドロップ
+      if(this.power === -1){
+        drawImage(dropEffectImages[1]);
+      }
+    }
+
+    const decorateLock = (drawDrop) => ()=>{
+      drawDrop();
+      if(this.lock){
+        drawImage(dropEffectImages[2]);
+      }
+    }
+
+    const drawDropImage = ()=>{
+      drawImage(dropImages[this.id]);
+    }
+
+    let drawDrop = drawDropImage;
+    //ドロップの強化を反映
+    drawDrop = decoratePower(drawDrop);
+    //ドロップをロック
+    drawDrop = decorateLock(drawDrop);
+    //持っているドロップなら半透明にする
+    drawDrop = decorateAlpha(hold?0.5:1, drawDrop);
+    
+    drawDrop();
+
+    
   }
 }
 
-export {Drop, dropImages};
+export {Drop, dropImages, dropEffectImages as powerImages};
