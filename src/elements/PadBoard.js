f0389d08e6ac3ca32819e43dfff22ba89f80f22b
OGURA DAIKI <dkog2001@gmail.com>
2023-02-12 16:09:09 +0900

    盤面の操作部分を分離

--- src/elements/PadBoard.js
+++ src/elements/PadBoard.js
@@ -1,18 +1,49 @@
 import { Drop, dropImages } from "../libs/Drops.js";
 import ReactiveStates from "../libs/ReactiveStates.js";
 
-const clamp = (min, x, max) => min > x?min:(x < max?x:max);
+const clamp = (min, x, max) => min > x ? min : (x < max ? x : max);
 
-const newBoard = (long, genDrop=() => new Drop(Math.floor(Math.random() * 6))) => [...Array(long-1)].map(() => [...Array(long)].map(genDrop));
+const newBoard = (long, genDrop = () => new Drop(Math.floor(Math.random() * 6))) => [...Array(long - 1)].map(() => [...Array(long)].map(genDrop));
 
 const bgColor = ['rgb(40, 20, 0)', 'rgb(60, 40, 0)'];
 
-const Position = obj=>{
-  if(!obj) return {empty:true};
-  const {x,y} = obj;
-  return {x,y};
+const Position = obj => {
+  if (!obj) return { empty: true };
+  const { x, y } = obj;
+  return { x, y };
 };
-const EmptyPosition = ()=>Position();
+const EmptyPosition = () => Position();
+
+const swap = (board, p1, p2) => {
+  const drop = board[p1.y][p1.x];
+  board[p1.y][p1.x] = board[p2.y][p2.x];
+  board[p2.y][p2.x] = drop;
+}
+
+const purifyObj = obj => Object.assign(Object.create(null), obj);
+const Pattern = obj => {
+  const keys = new Set(Object.keys(obj));
+  const purified = purifyObj(obj);
+  return purifyObj({
+    get:(name)=>{
+      if(keys.has(name)){
+        return purified[name];
+      }
+      else if(keys.has("default")){
+        return purified["default"];
+      }
+      return undefined;
+    },
+    do(name){
+      const action = this.get(name);
+      if(typeof action === "function"){
+        action();
+        return true;
+      }
+      return false;
+    }
+  });
+};
 
 class PADBoard extends HTMLElement {
 
@@ -46,8 +77,98 @@ class PADBoard extends HTMLElement {
   #ghost;
   #states;
   #tileSize = 128;
-  
+
   #pointerId = null;
+
+  #emulateMove(from, to) {
+    
+    if(to.empty){
+      return;
+    }
+    if(from.empty){
+      if(this.#mode === "palette"){
+        this.onPointerMoved(to, from);
+      }
+      return;
+    }
+
+    const pos = { ...from };
+    const before = { ...pos };
+
+    const factorX = pos.x > to.x ? -1 : 1;
+    const factorY = pos.y > to.y ? -1 : 1;
+
+    while (true) {
+      let hasDiff = false;
+      if (pos.x !== to.x) {
+        pos.x += factorX
+        hasDiff = true;
+      }
+      if (pos.y !== to.y) {
+        pos.y += factorY;
+        hasDiff = true;
+      }
+      if (!hasDiff) {
+        break;
+      }
+      this.onPointerMoved(pos, before);
+      before.x = pos.x;
+      before.y = pos.y;
+    }
+  }
+
+  dispatchDropPush(pos){
+    this.dispatchEvent(new CustomEvent(
+      "dropPushed",
+      {
+        detail:{target:this, pointerPos:pos},
+        composed:true, bubbles:true,
+      }
+    ));
+  }
+
+  onPointerMoved(nv, ov) {
+    if(nv.empty){
+      return;
+    }
+
+    const actions = Pattern({
+      palette:()=>this.dispatchDropPush(nv),
+      puzzle:()=>swap(this.#states.board, ov, nv),
+    });
+
+    actions.do(this.#mode);
+
+  }
+
+  get __defineStates() {
+    return {
+      size: {
+        value: 6,
+      },
+      board: {
+        value: newBoard(6),
+      },
+      pointerDown: {
+        value: false,
+      },
+      pointerPos: {
+        value: EmptyPosition(),
+        hasChanged: (nv, ov) => {
+          const hasChange = ["x", "y"].some(key => nv[key] !== ov[key]);
+          if(hasChange){
+            this.#emulateMove(ov, nv);
+          }
+          return hasChange;
+        },
+      },
+      disables: {
+        value: new Set(),
+      }
+    }
+  }
+
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
@@ -57,87 +178,7 @@ class PADBoard extends HTMLElement {
       <img id="ghost" draggable=false>
     `;
 
-    const move = (board, from, to) => {
-
-      if(from.empty){
-        return;
-      }
-      
-      const pos = {...from};
-      const before = {...pos};
-
-      const change = (p1, p2) => {
-        const drop = board[p1.y][p1.x];
-        board[p1.y][p1.x] = board[p2.y][p2.x];
-        board[p2.y][p2.x] = drop;
-      }
-
-      const factorX = pos.x > to.x?-1:1;
-      const factorY = pos.y > to.y?-1:1;
-
-      while(true){
-        let hasDiff = false;
-        if(pos.x !== to.x){
-          pos.x += factorX
-          hasDiff = true;
-        }
-        if(pos.y !== to.y){
-          pos.y += factorY;
-          hasDiff = true;
-        }
-        if(!hasDiff){
-          break;
-        }
-        if(this.#mode === "puzzle"){
-          change(before, pos);
-        }
-        else{
-          this.dispatchEvent(new CustomEvent(
-            "dropPushed",
-            {
-              detail:{target:this, pointerPos:pos},
-              composed:true, bubbles:true
-            }
-          ));
-        }
-        before.x = pos.x;
-        before.y = pos.y;
-      }
-    }
-
-    this.#states = ReactiveStates({
-      size: {
-        value: 6,
-      },
-      board: {
-        value: newBoard(6),
-      },
-      pointerDown: {
-        value:false,
-      },
-      pointerPos: {
-        value:EmptyPosition(),
-        hasChanged:(nv, ov) => {
-          const hasChange = ["x", "y"].some(key => nv[key] !== ov[key]);
-          if(!nv.empty && hasChange){
-            if(ov.empty && this.#mode === "palette"){
-              this.dispatchEvent(new CustomEvent(
-                "dropPushed",
-                {
-                  detail:{target:this, pointerPos:nv},
-                  composed:true, bubbles:true
-                }
-              ));
-            }
-            move(this.#states.board, ov, nv);
-          }
-          return hasChange;
-        },
-      },
-      disables:{
-        value:new Set(),
-      }
-    });
+    this.#states = ReactiveStates(this.__defineStates);
     this.#states.setCallback(() => this.render());
 
     this.#canvas = this.shadowRoot.querySelector("#canvas");
@@ -146,95 +187,95 @@ class PADBoard extends HTMLElement {
     this.size = 6;
 
     const beginPuzzle = e => {
-      if(this.#pointerId !== null){
+      if (this.#pointerId !== null) {
         return;
       }
       this.#pointerId = e.pointerId;
       const pointerPos = this.#getPointerTile(e);
       this.#states.updateStates({
-        pointerDown:true,
+        pointerDown: true,
         pointerPos
       });
       this.#drawGhost();
-      
-      if(this.#mode === "puzzle"){
+
+      if (this.#mode === "puzzle") {
         this.#moveGhost();
       }
     }
     this.#canvas.addEventListener("pointerdown", beginPuzzle);
     //this.#canvas.addEventListener("touchstart", beginPuzzle);
 
-    this.#canvas.addEventListener("touchmove", e=>{
+    this.#canvas.addEventListener("touchmove", e => {
       e.preventDefault();
     });
 
     const finishPuzzle = e => {
-      if(e.pointerId !== this.#pointerId){
+      if (e.pointerId !== this.#pointerId) {
         return;
       }
       this.#pointerId = null;
       this.#states.updateStates({
-        pointerDown:false,
-        pointerPos:EmptyPosition(),
+        pointerDown: false,
+        pointerPos: EmptyPosition(),
       });
       this.#moveGhost();
     }
     window.addEventListener("pointerup", finishPuzzle);
     //window.addEventListener("mouseup", finishPuzzle);
 
-    window.addEventListener("pointermove", e=>{
-      if(this.#pointerId !== e.pointerId){
+    window.addEventListener("pointermove", e => {
+      if (this.#pointerId !== e.pointerId) {
         return;
       }
-      if(this.#states.pointerDown){
+      if (this.#states.pointerDown) {
         this.#states.pointerPos = this.#getPointerTile(e);
       }
       this.#moveGhost();
     });
 
-    new ResizeObserver(()=>{
+    new ResizeObserver(() => {
       this.#rect = this.#canvas.getBoundingClientRect();
     }).observe(this);
   }
 
   #mode;
-  set mode(value){
+  set mode(value) {
     this.#mode = value;
   }
 
-  modifyDrop(pos, func){
+  modifyDrop(pos, func) {
     const drop = this.#states.board[pos.y][pos.x];
-    func({drop, board:this.#states.board});
+    func({ drop, board: this.#states.board });
     this.render();
   }
 
-  enableDrop(id){
+  enableDrop(id) {
     this.#states.disables.delete(id);
     this.render();
   }
-  disableDrop(id){
+  disableDrop(id) {
     this.#states.disables.add(id);
     this.render();
   }
-  isDisableDrop(id){
+  isDisableDrop(id) {
     return this.#states.disables.has(id);
   }
 
-  #raw=EmptyPosition();
+  #raw = EmptyPosition();
   #rect;
 
-  #getPointerTile(e){
-    if(e instanceof TouchEvent){
+  #getPointerTile(e) {
+    if (e instanceof TouchEvent) {
       e = e.touches[0];
     }
     const rect = this.#rect;
     this.#raw = {
-      x: e.pageX-rect.left,
-      y: e.pageY-rect.top,
+      x: e.pageX - rect.left,
+      y: e.pageY - rect.top,
     };
-    const x = clamp(0, Math.floor(this.#raw.x / this.#canvas.offsetWidth * this.size), this.size-1);
-    const y = clamp(0, Math.floor(this.#raw.y / this.#canvas.offsetHeight * (this.size-1)), this.size-2);
-    return Position({x,y});
+    const x = clamp(0, Math.floor(this.#raw.x / this.#canvas.offsetWidth * this.size), this.size - 1);
+    const y = clamp(0, Math.floor(this.#raw.y / this.#canvas.offsetHeight * (this.size - 1)), this.size - 2);
+    return Position({ x, y });
   }
 
   #start;
@@ -252,7 +293,7 @@ class PADBoard extends HTMLElement {
     });
   }
 
-  get size(){
+  get size() {
     return this.#states.size;
   }
 
@@ -271,7 +312,7 @@ class PADBoard extends HTMLElement {
     ctx.fillRect(0, 0, size * this.#tileSize, size * this.#tileSize);
 
     ctx.fillStyle = bgColor[1];
-    this.#loopTile(({top, left})=>{
+    this.#loopTile(({ top, left }) => {
       if ((left + top) % 2) {
         ctx.fillRect(left * this.#tileSize, top * this.#tileSize, this.#tileSize, this.#tileSize);
       }
@@ -280,39 +321,39 @@ class PADBoard extends HTMLElement {
 
   #drawDrop(ctx) {
     const pos = this.#states.pointerPos;
-    this.#loopTile(({top, left})=>{
+    this.#loopTile(({ top, left }) => {
       const hold = this.#mode === "puzzle" && pos.x === left && pos.y === top;
-      this.#states.board[top][left].draw(ctx, {size:this.#tileSize, x:left, y:top, hold, disables:this.#states.disables});
+      this.#states.board[top][left].draw(ctx, { size: this.#tileSize, x: left, y: top, hold, disables: this.#states.disables });
     });
   }
 
-  #drawGhost(){
+  #drawGhost() {
     //少し大きく表示
     const displayTileSize = this.#canvas.offsetWidth / this.size * 1.2;
     const sizes = {
-      width : `${displayTileSize}px`,
-      height : `${displayTileSize}px`,
+      width: `${displayTileSize}px`,
+      height: `${displayTileSize}px`,
     };
     const pos = this.#states.pointerPos;
-    if(!pos.empty){
+    if (!pos.empty) {
       Object.assign(this.#ghost.style, sizes);
       const drop = this.#states.board[pos.y][pos.x];
       drop.createGhost(this.#ghost);
     }
   }
 
-  #moveGhost(){
-    if(this.#mode === "palette"){
+  #moveGhost() {
+    if (this.#mode === "palette") {
       return;
     }
-    this.#ghost.style.display = this.#states.pointerDown ? "block":"none";
-    if(!this.#states.pointerDown){
+    this.#ghost.style.display = this.#states.pointerDown ? "block" : "none";
+    if (!this.#states.pointerDown) {
       return;
     }
     //少し大きく表示
     const displayTileSize = this.#canvas.offsetWidth / this.size * 1.2;
-    const offset = displayTileSize/2;
-    this.#ghost.style.transform = `translate(${this.#raw.x - offset}px,${this.#raw.y - offset*1.5}px)`;
+    const offset = displayTileSize / 2;
+    this.#ghost.style.transform = `translate(${this.#raw.x - offset}px,${this.#raw.y - offset * 1.5}px)`;
   }
 
   render() {
@@ -321,11 +362,11 @@ class PADBoard extends HTMLElement {
     this.#drawDrop(ctx);
   }
 
-  clearBoard(){
-    this.#states.board = newBoard(this.#states.size, ()=>new Drop(-1));
+  clearBoard() {
+    this.#states.board = newBoard(this.#states.size, () => new Drop(-1));
   }
-  random(){
-    this.#states.board = newBoard(this.#states.size, ()=>new Drop(Math.floor(Math.random()*6)))
+  random() {
+    this.#states.board = newBoard(this.#states.size, () => new Drop(Math.floor(Math.random() * 6)))
   }
 }
 customElements.define("pad-board", PADBoard);
